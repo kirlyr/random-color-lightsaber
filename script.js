7c1c2491e3498c2b5d938b12b75766661c120ea5
irfan tri atsal <irfan.basiru@gmail.com>
2023-08-23 20:32:25 +0700

    final code without sound

---
+++ script.js
@@ -0,0 +1,7 @@
+function getRandomColor() {
+   document.documentElement.style.setProperty('--blade-color', `${getRandomInt(10, 1000)}`);
+}
+
+function getRandomInt(min, max) {
+   return Math.floor(Math.random() * (max - min)) + min;
+}
